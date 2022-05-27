67551bd7331e31debb3239d7cc0f29358eedcc18
omaroo12 <omaradel3458@gmail.com>
2022-05-27 19:10:55 +0200

    GradesAnnouncment ClassesAdded

---
+++ src/Messages/GradesAnnouncement.java
@@ -0,0 +1,13 @@
+package Messages;
+
+public class GradesAnnouncement implements Message  {
+	
+	public String prepareMessage(String placeHolders[]) {
+		// code to replace place holders of this type
+		return "";
+	}
+    public boolean verifyGrades()
+    {
+         return true;
+    }
+}
