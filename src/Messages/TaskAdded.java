21232305dd7f87670cfa6f62a095d30d6859c65b
Omar Yasser <omaryasser21780@gmail.com>
2022-05-27 18:56:33 +0200

    Implemented changes in messages package

---
+++ src/Messages/TaskAdded.java
@@ -0,0 +1,14 @@
+package Messages;
+
+public class TaskAdded implements Message {
+    public String prepareMessage(String placeHolders[]) {
+		// code to replace place holders of this type
+		
+		return "";
+	}
+	
+	
+	public void addTeamDescription() {
+		
+	}
+}
