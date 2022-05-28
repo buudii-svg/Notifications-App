b4c9bc7f3513f59cb6a6681d3c5d7d349e302936
Msherifff <94564494+Msherifff@users.noreply.github.com>
2022-05-28 15:02:24 +0200

    removed verify grades functions from child classes

--- src/Messages/GradesAnnouncementEmailMessage.java
+++ src/Messages/GradesAnnouncementEmailMessage.java
@@ -1,15 +1,11 @@
 package Messages;
 
-public class GradesAnnouncementEmailMessage {
+public class GradesAnnouncementEmailMessage extends GradesAnnouncement {
 
 	public String prepareMessage(String placeHolders[]) {
 		// code to replace place holders of this type
 		return "";
 	}
 	
-	public boolean verifyGrades() {
-		// code to verify Grades before announcement
-		
-		return true;
-	}
+
 }
